5e37824979589842990fc49750722d08bef2d088
israromar <iisraromar@gmail.com>
2021-06-19 20:54:18 +0500

    updated

--- App.js
+++ App.js
@@ -6,19 +6,8 @@ import {PersistGate} from 'redux-persist/integration/react';
 
 import {store, persistor} from './src/store';
 import {AppNavigator} from './src/navigation';
-// import NotifService from './NotifService';
 
 export default class App extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {};
-
-  //   this.notif = new NotifService(
-  //     this.onRegister.bind(this),
-  //     this.onNotif.bind(this),
-  //   );
-  // }
-
   render() {
     return (
       <Provider store={store}>
